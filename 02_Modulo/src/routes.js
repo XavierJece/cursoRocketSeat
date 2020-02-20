5bc81d90c14009104e655aab2758349a6ef758fa
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-02-20 04:01:31 -0300

    Adicionando dependencipais para ajudar no Padrão de Código
    Adicionado Eslint -> AIRBNB
    Adicionado Prettier
    Adicionado editorConfig para ajudar em visualização em outro editor

--- 02_Modulo/src/routes.js
+++ 02_Modulo/src/routes.js
@@ -1,9 +1,9 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({message: "Hello Word!!"});
+  return res.json({ message: 'Hello Word!!' });
 });
 
 export default routes;
