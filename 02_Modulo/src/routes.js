34075813b167511920f0465166d988b2223ead89
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-04-07 11:25:03 -0300

    Criando e configurando upload do userAvatar

--- 02_Modulo/src/routes.js
+++ 02_Modulo/src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -13,5 +17,7 @@ routes.post('/users', UserController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
