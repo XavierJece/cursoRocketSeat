9eed32350392330b8dc538d4845dcd290ae4eeca
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-04-07 11:24:02 -0300

    Atualizando Credenciais da database

--- 02_Modulo/src/config/database.js
+++ 02_Modulo/src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect: 'postgres',
     host: 'localhost',
-    username: 'postgres',
-    password: 'root',
+    username: 'db_developer',
+    password: 'db_developerROOT',
     database: 'gobarber',
     define: {
         timestamps: true,
