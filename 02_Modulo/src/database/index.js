7e49754c410cdd89d4c3568523af1f0f17e939d6
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-04-13 12:49:48 -0300

    add Mongoose e conexão para o mongo

--- 02_Modulo/src/database/index.js
+++ 02_Modulo/src/database/index.js
@@ -1,15 +1,18 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
+import mongoConfig from '../config/mongo';
 
 const models = [User, File];
 
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -21,6 +24,20 @@ class Database {
                 model =>
                     model.associate && model.associate(this.connection.models)
             );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose
+            .connect(
+                `mongodb+srv://${mongoConfig.username}:${mongoConfig.password}@cluster0-8h5vf.mongodb.net/${mongoConfig.database}?retryWrites=true&w=majority`,
+                {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                    useFindAndModify: true,
+                }
+            )
+            .then(() => console.log('MongoDB connected...'))
+            .catch(err => console.log(err));
     }
 }
 
