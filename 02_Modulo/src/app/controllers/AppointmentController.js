9298f463f94ed7e0f0a6bb1639a4c4ad6d46e13e
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-04-14 10:26:59 -0300

    Adição de verificação de data

--- 02_Modulo/src/app/controllers/AppointmentController.js
+++ 02_Modulo/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -15,10 +16,10 @@ class AppointmentController {
         }
 
         const { provider_id, date } = req.body;
+
         /**
          * Check if provider_id is a provider
          */
-
         const isProvider = await User.findOne({
             where: {
                 id: provider_id,
@@ -26,17 +27,47 @@ class AppointmentController {
             },
         });
 
-        const appoitment = await Appointment.create({
-            user_id: req.userId,
-            provider_id,
-            date,
-        });
-
         if (!isProvider) {
             return res.status(401).json({
                 error: 'You can only create appointments with providers',
             });
         }
+
+        /**
+         * Check for past dates
+         */
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permitted' });
+        }
+
+        /**
+         * Check date availability
+         */
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkAvailability) {
+            return res
+                .status(400)
+                .json({ error: 'Appointment date is not available' });
+        }
+
+        const appoitment = await Appointment.create({
+            user_id: req.userId,
+            provider_id,
+            date,
+        });
 
         return res.json({ appoitment });
     }
