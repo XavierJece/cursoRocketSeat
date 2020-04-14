9444f10d451a1f47e3723c187bc3b4d8fc30cf8f
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-04-14 10:28:14 -0300

    Adição listagem de agendamento user logodo

--- 02_Modulo/src/routes.js
+++ 02_Modulo/src/routes.js
@@ -21,7 +21,9 @@ routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
